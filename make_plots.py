31a488dc10623ce265224596d45588883914a912
Mark Piper <mark.piper@colorado.edu>
2016-12-05 16:07:02 -0700

    Make combined surface and scatter plot

--- make_plots.py
+++ make_plots.py
@@ -1,8 +1,17 @@
 """Make plots of the results of Dakotathon experiments."""
 
+import os
 import numpy as np
-import matplotlib.pyplot as mpl
+from scipy.interpolate import griddata
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
+
+TRANGE = [12.8, 15.8]
+PRANGE = [1.4, 1.8]
+QSRANGE = [0.0, 6.0]
+
+plt.rcParams['mathtext.default'] = 'regular'
 
 
 def read_dat_header(dat_file):
@@ -22,8 +31,43 @@ def read_dat_file(dat_file):
     return np.loadtxt(dat_file, skiprows=1, unpack=True, usecols=rnames)
 
 
-def make_stacked_surface_plot():
-    pass
+def grid_samples(x, y, z):
+    xy = np.array([x, y])
+    xy_t = np.transpose(xy)
+    grid_x, grid_y = np.mgrid[TRANGE[0] : TRANGE[1] : complex(20),
+                              PRANGE[0] : PRANGE[1] : complex(20)]
+    grid_z = griddata(xy_t, z, (grid_x, grid_y), method='cubic')
+    return (grid_x, grid_y, grid_z)
+
+
+def make_stacked_surface_plot(experiment_dir, outfile='surface_plot.png'):
+    dat_file = os.path.join(experiment_dir, 'dakota.dat')
+    dat = read_dat_file(dat_file)
+
+    T = dat[1,]
+    P = dat[2,]
+    Qs = dat[3,]
+    gT, gP, gQs  = grid_samples(T, P, Qs)
+
+    fig = plt.figure()
+    ax = fig.gca(projection='3d')
+
+    ax.scatter(T, P, zs=0, s=10, zdir='z', c='r')
+    ax.plot_surface(gT, gP, gQs, rstride=1, cstride=1, linewidth=0.5)
+
+    ax.set_xlim(TRANGE)
+    ax.set_ylim(PRANGE)
+    ax.set_zlim(QSRANGE)
+    ax.set_autoscale_on(False)
+    ax.set_xlabel(r'$T\ [^{o}C]$')
+    ax.set_ylabel('$P\ [m\ yr^{-1}]$')
+    ax.set_zlabel('$Qs\ [kg\ s^{-1}]$')
+    plt.locator_params(axis='y', nbins=5)
+    plt.locator_params(axis='x', nbins=7)
+    ax.tick_params(axis='both', labelsize=10)
+
+    plt.savefig(outfile, dpi=150)
+    plt.close()
 
 
 def make_pdf_and_cdf_plot():
@@ -31,5 +75,6 @@ def make_pdf_and_cdf_plot():
 
 
 if __name__ == '__main__':
-    make_stacked_surface_plot()
+    experiment_dir = '/Users/mpiper/projects/AGU-2016/hydrotrend-sampling-study'
+    make_stacked_surface_plot(experiment_dir)
     make_pdf_and_cdf_plot()
